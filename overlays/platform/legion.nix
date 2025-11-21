8fbf55b5c30600d720fc59b907ae3cefa285ba10
flamarine <flamarine@outlook.com>
2025-11-21 10:24:22 +0800

    fix: attempt to fix machine optimizations

--- overlays/platform/legion.nix
+++ overlays/platform/legion.nix
@@ -1,11 +1,12 @@
 final: prev: {
-  final.stdenv = prev.stdenvAdapters.addAttrsToDerivation {
-    env.CFLAGS = "-O3 -march=native -mtune=native -pipe -flto=thin";
-    env.CXXFLAGS = "-O3 -march=native -mtune=native -pipe -flto=thin";
-    env.LDFLAGS = "-fuse-ld=mold -Wl,-O3 -Wl,--as-needed";
+  final.stdenv = prev.stdenvAdapters.impureUseNativeOptimizations (prev.stdenvAdapters.addAttrsToDerivation {
+    env.CFLAGS = "-O3 -mtune=native -pipe -flto=thin";
+    env.CXXFLAGS = "-O3 -mtune=native -pipe -flto=thin";
+    env.LDFLAGS = "-Wl,-O3 -Wl,--as-needed";
     env.RUSTFLAGS = "-C target-cpu=native";
     env.NIX_BUILD_CORES = "0";
-    env.NIX_CFLAGS_LINK = "-fuse-ld=mold -Wl,-O3 -Wl,--as-needed";
-    env.NIX_CFLAGS_COMPILE = "-O3 -march=native -mtune=native -pipe -flto=thin";
-  } prev.stdenv;
+    env.NIX_CFLAGS_LINK = "-Wl,-O3 -Wl,--as-needed";
+    env.NIX_CFLAGS_COMPILE = "-O3 -mtune=native -pipe -flto=thin";
+    env.MOLD_JOBS = "1";
+  } prev.stdenv);
 }
